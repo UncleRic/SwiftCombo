7f19555a031df3debc11e1d527167652680d0ca8
Frederick C. Lee <frederick.c.lee@gmail.com>
2016-04-22 17:25:41 -0700

    Raw tableview stub.

---
+++ SwiftCombo/CardCell.swift
@@ -0,0 +1,14 @@
+//  SwiftCombo
+//
+//  Created by Frederick C. Lee on 4/22/16.
+//  Copyright © 2016 Amourine Technologies. All rights reserved.
+// -----------------------------------------------------------------------------------------------------
+
+import Foundation
+import UIKit
+
+class CardCell:UITableViewCell {
+    @IBOutlet weak var suitLabel:UILabel!
+    @IBOutlet weak var rankLabel: UILabel!
+    @IBOutlet weak var cardImageView:UIImageView!
+}
---
+++ SwiftCombo/ClubsViewController.swift
@@ -0,0 +1,41 @@
+//  ClubsViewController.swift
+//  SwiftCombo
+//
+//  Created by Frederick C. Lee on 4/22/16.
+//  Copyright © 2016 Amourine Technologies. All rights reserved.
+// -----------------------------------------------------------------------------------------------------
+
+import UIKit
+
+let kCellIdentifier = "TimeLineCell"
+
+class ClubsViewController: UIViewController {
+
+    @IBOutlet weak var gTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Getting UITableViewCell template:
+        let myNib = UINib(nibName: "RicTableViewCell",bundle: nil)
+        gTableView.registerNib(myNib, forCellReuseIdentifier: kCellIdentifier)
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let noteCell = tableView.dequeueReusableCellWithIdentifier(kCellIdentifier, forIndexPath:indexPath) as! UITableViewCell
+        return noteCell
+    }
+
+}
+
